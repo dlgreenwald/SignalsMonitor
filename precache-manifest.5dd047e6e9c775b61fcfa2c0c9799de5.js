2a36fb3c05c67e16ed6c47f8308b8e95c1d1eb18
dlgreenwald <dlgreen@dlgreen.com>
2019-11-24 20:22:57 -0500

    Updates

---
+++ precache-manifest.5dd047e6e9c775b61fcfa2c0c9799de5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b03569db45e430c41da8aae8f4b0356a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "47153f797c0737af34f8",
+    "url": "/static/css/2.85cd294e.chunk.css"
+  },
+  {
+    "revision": "798b54f7f744ca473606",
+    "url": "/static/css/main.dfca195d.chunk.css"
+  },
+  {
+    "revision": "47153f797c0737af34f8",
+    "url": "/static/js/2.95094723.chunk.js"
+  },
+  {
+    "revision": "798b54f7f744ca473606",
+    "url": "/static/js/main.d1332a98.chunk.js"
+  },
+  {
+    "revision": "e196672c2f70a974ed38",
+    "url": "/static/js/runtime-main.079a9a1d.js"
+  }
+]);
